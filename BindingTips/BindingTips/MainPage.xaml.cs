b1b36832354fcba1fa7b550a9c9a55966cc16485
Youmna <youmnab94@gmail.com>
2025-02-13 22:28:48 -0500

    Starter code

---
+++ BindingTips/BindingTips/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using BindingTips.Views;
+
+namespace BindingTips
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btn_InfoPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new InfoPage());
+        }
+    }
+
+}
---
+++ BindingTips/BindingTips/Views/InfoPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BindingTips.Views;
+
+public partial class InfoPage : ContentPage
+{
+	public InfoPage()
+	{
+		InitializeComponent();
+	}
+}
