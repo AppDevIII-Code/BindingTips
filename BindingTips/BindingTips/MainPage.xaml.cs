bb8d6c4c6f4e74e6338a10c0ab819d3e7e5ddf7b
Youmna <youmnab94@gmail.com>
2025-03-17 17:15:53 -0400

    ViewModel possible implementation

--- BindingTips/BindingTips/MainPage.xaml.cs
+++ BindingTips/BindingTips/MainPage.xaml.cs
@@ -1,15 +1,15 @@
 ﻿using BindingTips.Models;
+using BindingTips.ViewModels;
 using BindingTips.Views;
 
 namespace BindingTips
 {
     public partial class MainPage : ContentPage
     {
-        public Bill Bill { get; set; } = new Bill();
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = this;
+            BindingContext = new MainViewModel();
        
         }
 
@@ -18,43 +18,6 @@ namespace BindingTips
             await Navigation.PushAsync(new InfoPage());
         }
 
-        private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            double tipRate = Math.Round(TipSlider.Value,0);
-            Console.WriteLine($"{tipRate} %");
-            Bill.TipRate = (decimal)tipRate;
-
-        }
-
-        private void TotalAmount_Entry_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            decimal totalAmount;
-            if (decimal.TryParse(TotalAmountEntry.Text, out totalAmount))
-            {
-                Console.WriteLine($"{totalAmount} $");
-                Bill.Amount = totalAmount;
-            }   
-        }
-
-        private void ProvincesPicker_SelectionChanged(object sender, EventArgs e)
-        {
-            if(ProvincesPicker.SelectedItem != null)
-            {
-                var province = ProvincesPicker.SelectedItem as Province;
-                Bill.TaxRate = province.TaxRate;
-            }
-
-        }
-
-        private void Btn_DecrementSplit_Clicked(object sender, EventArgs e)
-        {
-            Bill.Split--;
-        }
-
-        private void Btn_IncrementSplit_Clicked(object sender, EventArgs e)
-        {
-            Bill.Split++;
-        }
     }
 
 }
---
+++ BindingTips/BindingTips/ViewModels/MainViewModel.cs
@@ -0,0 +1,99 @@
+﻿using BindingTips.Models;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace BindingTips.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Interface implementation
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Observed Properties
+        private Bill _bill = new Bill();
+        public Bill Bill { 
+            get => _bill;
+            set
+            {
+                _bill = value;
+                OnPropertyChanged(nameof(Bill));
+            }
+        }
+        private Province _selectedProvince;
+        public Province SelectedProvince
+        {
+            get => _selectedProvince;
+            set
+            {
+                _selectedProvince = value;
+                Bill.TaxRate = _selectedProvince.TaxRate;
+                OnPropertyChanged(nameof(SelectedProvince));
+            }
+        }
+
+        public double TipValueInput
+        {
+            get => (double)Bill.TipRate;
+            set
+            {
+                Bill.TipRate = (decimal)Math.Round(value, 0);
+                OnPropertyChanged(nameof(TipValueInput));
+            }
+        }
+        public string AmountTextInput { 
+            get => Bill.Amount.ToString();
+            set
+            {
+                decimal totalAmount;
+                if (decimal.TryParse(value, out totalAmount))
+                {
+                    Console.WriteLine($"{totalAmount} $");
+                    Bill.Amount = totalAmount;
+                    OnPropertyChanged(nameof(AmountTextInput));
+                }
+                else if(string.IsNullOrEmpty(value))
+                {
+                    Bill.Amount = 0m;
+                    OnPropertyChanged(nameof(AmountTextInput));
+                }
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand IncreaseSplit { get; set; }
+        public ICommand DecreaseSplit { get; set; }
+
+        #endregion
+
+
+        public MainViewModel()
+        {
+
+            IncreaseSplit = new Command(IncreaseExecute);
+            DecreaseSplit = new Command(DecreaseExecute);
+            Bill = new Bill();
+        }
+
+        #region Commands methods
+  
+   
+        private void IncreaseExecute()
+        {
+            Bill.Split++;
+
+        }
+
+        private void DecreaseExecute()
+        {
+            Bill.Split--;
+
+        }
+        #endregion
+    }
+}
